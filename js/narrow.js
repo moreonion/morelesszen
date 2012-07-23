967e54d244d40b859f173b1621382edfb34835b8
katrin <katrin@moreonion.com>
2012-07-23 15:13:37 +0200

    renamed ae theme in ae_base

---
+++ js/narrow.js
@@ -0,0 +1,22 @@
+function checkNarrow(){
+	var size = {
+		four: 400,
+		five: 500,
+		six: 600,
+		sixfive: 650,
+		seven: 700,
+		sevensome: 768,
+		eightfive: 850,
+		nine: 900,
+	};
+	jQuery.each(size, function(cls, size) {
+		if (jQuery(window).width() >= size) {
+			jQuery('html').addClass(cls);
+		} else {
+			jQuery('html').removeClass(cls);
+		}
+	});
+}
+
+jQuery(window).resize(checkNarrow);
+jQuery(window).ready(checkNarrow);
