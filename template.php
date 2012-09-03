4946717430d71bd20130f1a60ddf8ef2bb6a87bc
katrin <katrin@moreonion.com>
2012-09-03 17:00:41 +0200

    neue klasse zu field-item hinzugefügt die klärt ob video oder bild issue#1718

--- template.php
+++ template.php
@@ -499,3 +499,29 @@ function ae_base_preprocess_menu_link(&$variables) {
     }
   }
 }
+
+/**
+ * Implements template_preprocess_field() for main pic/vid
+ */
+ 
+function ae_base_field__image($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even') . ' media-type-' . $item['#bundle'];
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
