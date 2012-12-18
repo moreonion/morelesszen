7632814bbbd9748873d83ae45fb450210ae6c44c
Alex Berger <alex@more-onion.com>
2012-12-18 20:11:06 +0100

    indentation

--- ae_starterkit/js/narrow.js
+++ ae_starterkit/js/narrow.js
@@ -1,21 +1,15 @@
 function checkNarrow(){
-	var size = {
-		four: 400,
-		five: 500,
-		six: 600,
-		sixfive: 650,
-		seven: 700,
-		sevensome: 768,
-		eightfive: 850,
-		nine: 900
-	};
-	jQuery.each(size, function(cls, size) {
-		if (jQuery(window).width() >= size) {
-			jQuery('html').addClass(cls);
-		} else {
-			jQuery('html').removeClass(cls);
-		}
-	});
+  var size = {
+    tablet: 768,
+    desktop: 950
+  };
+  jQuery.each(size, function(cls, size) {
+    if (jQuery(window).width() >= size) {
+      jQuery('html').addClass(cls);
+    } else {
+      jQuery('html').removeClass(cls);
+    }
+  });
 }
 
 jQuery(window).resize(checkNarrow);
