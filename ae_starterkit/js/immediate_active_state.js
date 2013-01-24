7635d0f36b5126eadb2bc9f5ba1b4c569bb57d8f
Alex Berger <alex@more-onion.com>
2013-01-24 17:08:39 +0100

    retab

--- ae_starterkit/js/immediate_active_state.js
+++ ae_starterkit/js/immediate_active_state.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function($) {
-	var nav = $('#main-menu, .menu');
-	var menuItems = $('li', nav);
-	menuItems.each(function() {
-		var item = $(this);
-		$('a', this).click(function(obj){
-			menuItems.each(function(){
-				$(this).removeClass('active').removeClass('active-trail');
-				$('a', this).removeClass('active').removeClass('active-trail');
-			});
-			item.addClass('active').addClass('active-trail');
-			$(this).addClass('active').addClass('active-trail');
-		});
-	});
+  var nav = $('#main-menu, .menu');
+  var menuItems = $('li', nav);
+  menuItems.each(function() {
+    var item = $(this);
+    $('a', this).click(function(obj){
+      menuItems.each(function(){
+        $(this).removeClass('active').removeClass('active-trail');
+        $('a', this).removeClass('active').removeClass('active-trail');
+      });
+      item.addClass('active').addClass('active-trail');
+      $(this).addClass('active').addClass('active-trail');
+    });
+  });
 });
 
--- js/close_messages.js
+++ js/close_messages.js
@@ -1,21 +1,21 @@
 ae_base.behaviors.close_messages = {
-	attach: function(context, settings) {
-		(function ($) {
-			/* =============================================================================
-				 Add 'x' close button and handler to status messages.
-				 ========================================================================== */
-			$.fn.closeButtonMessages = function() {
-				$('.messages').each(function() {
-					if ($(this).find('a.close').length < 1) {
-						$(this).prepend('<a class="close" href="#" title="' + ae_base.t('Close') + '">x</a>');
-					}
-				});
-				$('.messages a.close').click(function(e) {
-					e.preventDefault();
-					$(this).parent().fadeOut('slow');
-				});
-			};
-			$().closeButtonMessages();
-		})(jQuery);
-	}
+  attach: function(context, settings) {
+    (function ($) {
+      /* =============================================================================
+         Add 'x' close button and handler to status messages.
+         ========================================================================== */
+      $.fn.closeButtonMessages = function() {
+        $('.messages').each(function() {
+          if ($(this).find('a.close').length < 1) {
+            $(this).prepend('<a class="close" href="#" title="' + ae_base.t('Close') + '">x</a>');
+          }
+        });
+        $('.messages a.close').click(function(e) {
+          e.preventDefault();
+          $(this).parent().fadeOut('slow');
+        });
+      };
+      $().closeButtonMessages();
+    })(jQuery);
+  }
 }
