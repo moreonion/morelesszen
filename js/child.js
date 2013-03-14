2bdad64484ae7ee4bd30334a2840b7d9feb9fb9e
Alex Berger <alex@more-onion.com>
2013-03-14 17:58:17 +0100

    rename and retab #1709

--- js/child.js
+++
@@ -1,9 +0,0 @@
-var messageParent = function(scrollTop){
-		var h = document.body.scrollHeight;
-		h = (scrollTop)? h+'s':h;
-		if(top.postMessage){
-			top.postMessage( h , '*');
-		} else {
-			window.location.hash = 'h'+h;
-		}
-}
