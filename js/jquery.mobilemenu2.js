26e67484ebf24d34811e6991a408cb2c929c9250
Alex Berger <alex@more-onion.com>
2014-04-03 16:58:45 +0200

    mobile menu hide/show + style #3561

--- js/jquery.mobilemenu2.js
+++ js/jquery.mobilemenu2.js
@@ -6,48 +6,162 @@
  * - use more classes in corresponding css for fullheight, shift,
  */
 (function ( $ ) {
+  'use strict';
   $.fn.mobilemenu = function( options ) {
 
     var settings = $.extend({}, $.fn.mobilemenu.defaults, options );
-    var $self = $(this);
+    var mobileQuery = window.matchMedia('(min-width: 780px)');
+    var $menu = $(this),
+        $body = $('body'),
+        $iconContainer = $(settings.iconContainer),
+        $closeContainer = $(settings.closeContainer),
+        $icon,
+        $close;
 
-    // generate buttons hidden
+    $body.addClass(settings.mobileMenuEnabledClass);
+
+    // generate buttons or use elements/containers from settings
+    // default: before icon to menu, append close to menu
     if (settings.createIcon) {
-    console.log('create');
-      $.fn.mobilemenu.$icon = $('<a href="#">' + settings.iconText + '</a>').attr(settings.iconAttributes);
-      $(settings.iconContainer).append($.fn.mobilemenu.$icon);
-      //$.fn.mobilemenu.$icon.hide();
-    }
-    else {
-      // TODO
+      $icon = $('<a href="#">' + settings.iconText + '</a>').attr(settings.iconAttributes);
+      if ($iconContainer.length > 0) {
+        $iconContainer.append($icon);
+      } else {
+        $menu.before($icon);
+      }
+    } else {
+      $icon = $(settings.iconElement);
     }
     if (settings.createIcon) {
-      $.fn.mobilemenu.$close = $('<a href="#">' + settings.closeText + '</a>').attr(settings.closeAttributes);
-      this.prepend($.fn.mobilemenu.$close);
-      $.fn.mobilemenu.$close.hide();
-    }
-    else {
-      // TODO
+      $close = $('<a href="#">' + settings.closeText + '</a>').attr(settings.closeAttributes);
+      if ($closeContainer.length > 0) {
+        $closeContainer.append($close);
+      } else {
+        $menu.append($close);
+      }
+    } else {
+      $close = $(settings.closeElement);
     }
 
+    // gets called when switched to mobile
+    // sets up the classes, ...
+    var switchToMobile = function(mql) {
+      $menu.hide();
+      $icon.show();
+      $close.show();
+      $body.addClass(settings.mobileMenuClass);
+    }
+    // gets called when switched to desktop
+    var switchToDesktop = function(mql) {
+      $menu.show();
+      $icon.hide();
+      $close.hide();
+      $body.removeClass(settings.mobileMenuClass);
+    }
+
+    var setMenu = function (mql) {
+      if (mql.matches) {
+        switchToDesktop(mql);
+      } else {
+        switchToMobile(mql);
+      }
+    }
+
+    // add breakpoint listener and do inital call
+    mobileQuery.addListener(function(mql) {
+      setMenu(mql);
+    });
+    setMenu(mobileQuery);
+
+    var clickHandler = function (e) {
+      var width = $menu.innerWidth();
+      var animation = {};
+
+      if ($menu.is(':visible')) {
+        settings.beforeClose.call($menu, settings);
+
+        //if (settings.shiftBodyAside) {
+        //  animation[settings.animationFromDirection] = '0px';
+        //  $body.animate(animation, settings.animationDuration, function() {
+        //    $menu.hide();
+        //    $body.removeClass(settings.mobileMenuOpenClass);
+
+        //    settings.afterClose.call($menu, settings);
+        //  });
+        //}
+        //else {
+          animation[settings.animationFromDirection] = '-' + width + 'px';
+          $menu.animate(animation, settings.animationDuration, function() {
+            $menu.hide();
+            $body.removeClass(settings.mobileMenuOpenClass);
+
+            settings.afterClose.call(self, settings);
+          });
+        // }
+        // reset to prevent unexpected reuse of former values
+        animation = {};
+      } else {
+        settings.beforeOpen.call($menu, settings);
+
+        if (settings.adaptFullHeightOnResize) {
+          setMainMenuMinHeight($body.innerHeight());
+        }
+        //if (settings.shiftBodyAside) {
+        //  animation[settings.animationFromDirection] = '-' + width + 'px';
+        //  $menu.css(animation).show();
+        //  animation[settings.animationFromDirection] = width + 'px';
+        //  $body.addClass(settings.mobileMenuOpenClass).animate(animation, settings.animationDuration, function() {
+        //    settings.afterOpen.call($menu, settings);
+        //  });
+        //}
+        // else {
+          animation[settings.animationFromDirection] = '-' + width + 'px';
+          $menu.css(animation).show();
+          $body.addClass(settings.mobileMenuOpenClass);
+          animation[settings.animationFromDirection] = '0px';
+          $menu.animate(animation, settings.animationDuration, function() {
+            settings.afterOpen.call($menu, settings);
+          });
+        // }
+        // reset to prevent unexpected reuse of former values
+        animation = {};
+      }
+
+      $(this).blur();
+      e.preventDefault();
+      return false;
+    }
+
+    // bind click handler 
+    if ($icon.length > 0) {
+      $icon.on('click.mobilemenu', clickHandler);
+    }
+    if ($close.length > 0) {
+      $close.on('click.mobilemenu', clickHandler);
+    }
+
+    return this;
   }
+
 
   $.fn.mobilemenu.defaults = {
     // These are the defaults.
-    breakpoint: 400,
+    breakpoint: 780,
     breakpointTest: function () {},
     createIcon: true,
     iconText: 'Menu',
     iconContainer: '',
-    iconElement: '', // TODO
+    iconElement: undefined,
     iconAttributes: { id: 'mobile-menu-icon' },
     createClose: true,
     closeText: 'close',
     closeContainer: '',
-    closeElement: '', // TODO
+    closeElement: undefined,
     closeAttributes: { id: 'mobile-menu-close' },
+    mobileMenuClass: 'gone-mobile',
+    mobileMenuEnabledClass: 'with-mobile-menu',
     mobileMenuOpenClass: 'mobile-menu-open',
-    adaptFullHeightOnResize: true,
+    adaptFullHeightOnResize: false,
     animationDuration: 300,
     animationFromDirection: 'right',
     shiftBodyAside: true,
--- js/mobilemenu2.js
+++ js/mobilemenu2.js
@@ -1,71 +1,15 @@
 
 (function($) {
-  $.fn.mobilemenu.defaults.onSwitchToDesktop = function () {
-    var mobileMenu = $.fn.mobilemenu.$menu;
-
-    // move search form back into tools menu
-    searchForm =  $('.search-form', mobileMenu).detach();
-    $('.heading-first .tools').append(searchForm);
-
-    mobileMenu.css({minHeight: '0px'});
-    // reset the display, i.e. remove the element style
-    // otherwise the dropdown css in header.css will not work properly
-    // after the mobilemenu was used
-    $('.drop-holder', mobileMenu).css('display', '');
-    mobileMenu.css('overflow', '');
-  }
-
-  $.fn.mobilemenu.defaults.beforeOpen = function (menu) {
-    // move search form into mobile menu
-    var searchForm =  $('.tools .search-form').detach();
-    $('#nav', menu).before(searchForm);
-  }
-
-  $.fn.mobilemenu.defaults.init = function () {
-    var mobileMenu = $.fn.mobilemenu.$menu;
-    // add class has-submenu on li
-    $('li ul', mobileMenu).closest('li').addClass('has-submenu');
-    $('li.has-submenu > a', mobileMenu).append('<span class="submenu-icon">v</span>');
-  }
-
-  // // custom collapsible
-  // // if the link clicked has a ul.menu sibling (i.e. a submenu)
-  // // we want to show the submenu
-  // $('html').on('click', '.mobilemenu-open #main-nav li a .submenu-icon', function(e) {
-  //   var a = $(this).parent();
-  //   var li = a.closest('li.has-submenu');
-  //   var ul = a.siblings('ul');
-  //   var holder = a.siblings('.drop-holder');
-  //   if (holder.length > 0) {
-  //     ul = $('.drop-content > ul', holder);
-  //   }
-  //   if (ul.length > 0) {
-  //     if (ul.is(':visible')) {
-  //       ul.hide();
-  //       $(this).removeClass('submenu-open');
-  //     } else {
-  //       ul.show();
-  //       $(this).addClass('submenu-open');
-  //     }
-  //     // lose focus
-  //     a.blur();
-  //     // stop propagation - we do not want to follow link when click on submenu-icon
-  //     e.stopPropagation();
-  //     return false;
-  //   }
-  // });
-
-})(jQuery);
-
-(function($) {
 Drupal.behaviors.mobilemenu = {};
 Drupal.behaviors.mobilemenu.attach = function(context, settings) {
   if ($('html').hasClass('lt-ie9')) {
     return;
   } else {
-    $('#main-menu').mobilemenu({
-      iconContainer: '#header',
-      closeContainer: '#main-nav'
+    $('#main-menu', context).mobilemenu({
+      beforeOpen: function (settings) {
+        // test before call
+        console.log(this);
+      }
     });
   }
 };
