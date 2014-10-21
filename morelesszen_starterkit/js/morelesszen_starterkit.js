cf3ae30199c5977a6d05ae0467b42a1e0a6168bc
Alex Berger <alex@more-onion.com>
2014-10-21 13:37:08 +0200

    add webform ajax slide to starterkit

--- morelesszen_starterkit/js/morelesszen_starterkit.js
+++ morelesszen_starterkit/js/morelesszen_starterkit.js
@@ -23,6 +23,15 @@ Drupal.behaviors.morelesszen_starterkit.attach = function(context, settings) {
     });
   }
 
+  // container id begins with webform-ajax-wrapper
+  if (context == document) {
+    $('*[id^=webform-ajax-wrapper]').once(function(){$(this).webformAjaxSlide({
+      loadingDummyMsg: Drupal.t('loading'),
+      onSlideBegin: function (ajaxOptions) {},
+      onSlideFinished: function (ajaxOptions) {},
+      onLastSlideFinished: function (ajaxOptions) {}
+    })});
+  }
 
 };
 
