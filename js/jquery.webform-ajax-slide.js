105a8a90df74b75872ebaf9322aa179ad0119ef3
Roman Zimmermann <roman@more-onion.com>
2016-08-30 08:40:04 +0200

    Another webform-ajax-slide rework.

--- js/jquery.webform-ajax-slide.js
+++ js/jquery.webform-ajax-slide.js
@@ -7,15 +7,17 @@
   $.fn.webformAjaxSlide = function( options ) {
 
     // bail if we are not called on an AJAX enabled webform.
-    var $container = $(this).parents('[id^=webform-ajax-wrapper]').first();
-    if (!$container.length) {
+    var $ajaxWrapper = $(this).parents('[id^=webform-ajax-wrapper]').first();
+    if (!$ajaxWrapper.length) {
       return this;
     }
+    var ajax_id = $ajaxWrapper.attr('id');
 
     // These are the defaults.
     var defaults = {
       minHeight: '0',
-      wrapperClass: 'webform-ajax-slide-wrapper',
+      containerClass: 'webform-ajax-slide-wrapper',
+      slideClass: 'webform-ajax-slide',
       loadingDummyClass: 'webform-ajax-slide-loading-dummy',
       loadingDummyMsg: 'loading',
       onSlideFinished: function () {},
@@ -30,58 +32,44 @@
 
     // generate an wrapper around the ajax wrapper to
     // prevent elements visually sliding over the page
-    var $containerWrapper = $(this).parents('.' + settings.wrapperClass).first();
-    if ($containerWrapper.length < 1) {
-      $containerWrapper = $('<div class="'+settings.wrapperClass+'">');
-      $containerWrapper.css({position: 'relative'});
-      $container.wrap($containerWrapper);
+    var $container = $(this).parents('.' + settings.containerClass).first();
+    if ($container.length < 1) {
+      // Generate our containers if needed.
+      $container = $('<div class="'+settings.containerClass+'">');
+      $container.css({position: 'relative'});
+      $ajaxWrapper.wrap($container);
+      $container = $ajaxWrapper.parent();
+
+      // generate a dummy div to display while loading
+      $('<div class="'+settings.loadingDummyClass+'"><div class="container"><div>' + settings.loadingDummyMsg + '</div></div></div>')
+        .hide().insertBefore($ajaxWrapper);
+
+      $ajaxWrapper.wrap($('<div class="'+settings.slideClass+'">'));
     }
 
-    // generate a dummy div to display while loading
-    var $loadingdummy = $containerWrapper.find('.' + settings.loadingDummyClass).first();
-    if (!$loadingdummy.length) {
-      var $loadingdummy = $('<div class="'+settings.loadingDummyClass+'"><div class="container"><div>' + settings.loadingDummyMsg + '</div></div></div>');
-      $loadingdummy.hide();
-      $loadingdummy.insertBefore($container);
-    }
-
-    // need to set an beforeSubmit callback to get the
-    // direction from a data attribute from the markup
-    var processTriggers = function(triggers) {
-      $.each(triggers, function(t) {
-        var trigger = triggers[t];
-        // leave non webform ajax triggers alone
-        if (trigger['callback'] != 'webform_ajax_callback') {
-          return;
-        }
-        trigger['beforeSubmit'] = function(form_values, element_settings, options) {
-          options["ajaxSlidingDirection"] = $(trigger.element).data("direction");
-        }
-      });
-    }
-    // initialize
-    processTriggers(Drupal.ajax);
+    var $loadingdummy = $('.' + settings.loadingDummyClass, $container).first();
+    var $slide = $ajaxWrapper.parents('.' + settings.slideClass).first();
 
     // called via ajaxSend()
     var onSend = function(ajax, ajaxOptions) {
       var targetPageNum;
       // set container anew (from loaded data)
-      var pageNum = parseInt($('input[name="details\[page_num\]"]', $container).attr('value'));
+      var pageNum = parseInt($('input[name="details\[page_num\]"]', $ajaxWrapper).attr('value'));
       ajax.onLastSlide = (pageNum == maxPageNum);
 
-      settings.onSlideBegin.call($container, ajaxOptions);
+      settings.onSlideBegin.call($ajaxWrapper, ajaxOptions);
 
       // after possible tampering of context + options by callback
       var stepForward = ajaxOptions["ajaxSlidingDirection"] == "back" ? false : true;
       var context = ajaxOptions.data;
 
-      //if ($containerWrapper.height() > minHeight) {
-      //  minHeight = $containerWrapper.height();
-      //  $containerWrapper.css({minHeight: minHeight});
+      //if ($container.height() > minHeight) {
+      //  minHeight = $container.height();
+      //  $container.css({minHeight: minHeight});
       //}
 
       // set dummy dimensions to current container dimensions
-      $loadingdummy.css({height: $container.height() + 'px', width: $container.width() + 'px'});
+      $loadingdummy.css({height: $slide.height() + 'px', width: $slide.width() + 'px'});
 
       // define the animation and it's "reverse"
       var anim = {};
@@ -89,59 +77,54 @@
       // one element needs position: relative
       if (stepForward) {
         $loadingdummy.css({position: 'absolute', right: '-120%'});
-        $container.css({position: 'relative', right: '', left: '0%'});
+        $slide.css({position: 'relative', right: '', left: '0%'});
         anim = {left: '-150%'};
         reverseAnim = {right: '0%'};
       } else {
         $loadingdummy.css({position: 'absolute', left: '-120%'});
-        $container.css({position: 'relative', right: '0%', left: ''});
+        $slide.css({position: 'relative', right: '0%', left: ''});
         anim = {right: '-150%'};
         reverseAnim = {left: '0%'};
       }
 
       // do the slide!
       // set container overflow to hidden to prevent overlapping¬
-      $containerWrapper.css({overflow: 'hidden'});
+      $container.css({overflow: 'hidden'});
       // move dummy in
       $loadingdummy.show().animate(reverseAnim, 800);
       // move container out
-      $container.animate(anim, 800, function() {});
+      $slide.animate(anim, 800, function() {});
     };
 
     var onSuccess = function(ajax, response) {
       // Don't slide-in if a redirect is in progress.
       for (var i=0; i<response.length; i++) {
         if (response[i].command == 'redirect') {
-          return;
+          return
         }
       }
-      $container.css({left: '', right: '', position: 'absolute', opacity: 0});
-      $loadingdummy.css('position', 'relative');
 
+      $slide.stop().css({left: '', right: '', position: 'absolute', opacity: 0});
+      $loadingdummy.css('position', 'relative');
+      $container.css({overflow: 'visible'});
       // to the incoming slide
-      $loadingdummy.animate({height: $container.height()}, 200, 'swing', function() {
-        $container.css('position', 'relative');
+      $loadingdummy.animate({height: $slide.height()}, 200, 'swing', function() {
+        $slide.css('position', 'relative');
         $loadingdummy.css('position', 'absolute').fadeOut(400);
-        $container.animate({opacity: 1}, 400, function() {
+        $slide.animate({opacity: 1}, 400, function() {
           $loadingdummy.hide();
-          $containerWrapper.css({overflow: 'visible'});
         });
       });
 
-      //if ($containerWrapper.height() > minHeight) {
-      //  minHeight = $containerWrapper.height();
-      //  $containerWrapper.css({minHeight: minHeight});
-      //}
-
       if (ajax.onLastSlide) {
         // call finishing callback
-        settings.onLastSlideFinished.call($container, response);
+        settings.onLastSlideFinished.call($ajaxWrapper, response);
       } else {
-        settings.onSlideFinished.call($container, response);
+        settings.onSlideFinished.call($ajaxWrapper, response);
       }
     }
 
-    var ajax_id = $container.attr('id');
+    // Drupal.ajax is keyed by button ids. We have to loop to find ours.
     $.each(Drupal.ajax, function() {
       if (this.wrapper.substr(1) == ajax_id) {
         var ajax = this;
@@ -156,16 +139,21 @@
         };
 
         this.success = function(response, status) {
-          onSuccess(ajax, response);
-          // recreate beforeSubmit callback on newly loaded elements
-          processTriggers(Drupal.ajax);
           old_success.call(ajax, response, status);
+          onSuccess(ajax, response);
         };
+
+        // set an beforeSubmit callback to get the
+        // direction from a data attribute from the markup
+        ajax['beforeSubmit'] = function(form_values, element_settings, options) {
+          options['ajaxSlidingDirection'] = $(this.element).data('direction');
+        }
       }
     });
 
     return this;
   }
+
 })(jQuery);
 
 
